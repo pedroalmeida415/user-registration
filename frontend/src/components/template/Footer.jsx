3e1933d99344a00620a1f96b0dd4685b3df9db81
Pedro Almeida <54154701+pedroalmeida415@users.noreply.github.com>
2019-08-21 19:32:27 -0300

    user-crud

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido por<strong> Pedro <span className="text-danger">Almeida</span></strong>
+        </span>
+    </footer>
